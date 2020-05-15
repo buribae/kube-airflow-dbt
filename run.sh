fa13fe5d901cef530bb95d2072abc83f70052506
Stephen Jang <stephen.jang@slalom.com>
2020-05-15 15:30:28 -0400

    Merge clean.sh with run.sh. Correct dags folder. Make log.sh to show last log only

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
+#/bin/bash
+
+# Expected Input:
+# $1: dag_name
+# $2: task_name
+
+# clean up dag run
+docker exec $AIRFLOW_CONTAINER airflow clear -c $1
+
+# run airflow dag
 docker exec $AIRFLOW_CONTAINER airflow run $1 $2 2020-01-01
+
