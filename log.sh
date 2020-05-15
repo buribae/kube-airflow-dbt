85dc05a3c45fb9066e1d3465ab4ecdae3428765b
Stephen Jang <stephen.jang@slalom.com>
2020-05-15 16:00:07 -0400

    rename dbt task with underscore

--- log.sh
+++ log.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-LOG_DIR="/usr/local/airflow/logs/dbt_kube_dag/dbt-debug/*"
+LOG_DIR="/usr/local/airflow/logs/dbt_kube_dag/dbt_debug/*"
 LAST_LOG=$(docker exec $AIRFLOW_CONTAINER /bin/bash -cx "cd $LOG_DIR && ls | tail -1")
 
 docker exec $AIRFLOW_CONTAINER /bin/bash -cx "cat $LOG_DIR/$LAST_LOG"
